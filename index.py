eb0b6f10f1e95a2f7fef5b7b6ff70b40917ca8bb
Alen Kumar Biswal <iswarb232@gmail.com>
2026-04-25 11:09:55 +0530

    Initial commit

--- index.py
+++ index.py
@@ -541,4 +541,11 @@ def generate():
     })
 
 if __name__ == '__main__':
+    import threading
+    import webbrowser
+
+    def open_browser():
+        webbrowser.open('http://127.0.0.1:5000')
+
+    threading.Timer(1.0, open_browser).start()
     app.run(debug=True, host='0.0.0.0', port=5000)
